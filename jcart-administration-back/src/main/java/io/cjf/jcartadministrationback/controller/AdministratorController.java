05eb1c0af6832fb59f8e5f2430bee653a36b0348
wdc <1650428523@qq.com>
2020-02-25 15:38:05 +0800

    add

---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
@@ -0,0 +1,59 @@
+package io.cjf.jcartadministrationback.controller;
+
+import io.cjf.jcartadministrationback.dto.in.*;
+import io.cjf.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import io.cjf.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.cjf.jcartadministrationback.dto.out.AdministratorShowOutDTO;
+import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/administrator")
+public class AdministratorController {
+
+    @GetMapping("/login")
+    public String login(AdministratorLoginInDTO administratorLoginInDTO){
+        return null;
+    }
+
+    @GetMapping("/getProfile")
+    public AdministratorGetProfileOutDTO getProfile(Integer adminstratorId){
+        return null;
+    }
+
+    @PostMapping("/updateProfile")
+    public void updateProfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
+
+    }
+
+    @GetMapping("/getPwdResetCode")
+    public String getPwdResetCode(@RequestParam String email){
+        return  null;
+    }
+
+    @PostMapping("/resetPwd")
+    public void resetPwd(@RequestBody AdministratorResetPwdInDTO administratorResetPwdInDTO){
+
+    }
+
+    @GetMapping("/getList")
+    public PageOutDTO<AdministratorListOutDTO> getList(@RequestParam Integer pageNum){
+        return  null;
+    }
+
+    @GetMapping("/getById")
+    public AdministratorShowOutDTO getById(@RequestParam Integer administratorId){
+        return  null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody AdministratorCreateInDTO administratorCreateInDTO){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody AdministratorUpdateInDTO administratorUpdateInDTO){
+
+    }
+
+}
