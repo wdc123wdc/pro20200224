67549d0ab478d2c9fdaa91092dc72393c6c687be
wdc <1650428523@qq.com>
2020-02-25 15:52:25 +0800

    add product list

---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ProductController.java
@@ -0,0 +1,21 @@
+package io.cjf.jcartadministrationback.controller;
+
+import io.cjf.jcartadministrationback.dto.in.ProductSearchImDTO;
+import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
+import io.cjf.jcartadministrationback.dto.out.ProductListOutDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/search")
+    public PageOutDTO<ProductListOutDTO> search (ProductSearchImDTO productSearchImDTO,
+    @RequestParam Integer pageNum
+    ){
+        return  null;
+    }
+}
