7d0d6d3ac6532adb51426893726feb63cda26a11
wdc <1650428523@qq.com>
2020-03-05 21:58:53 +0800

    Add   a front-end order history

---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/impl/OrderHistoryServiceImpl.java
@@ -0,0 +1,42 @@
+package io.cjf.jcartadministrationback.service.impl;
+
+import io.cjf.jcartadministrationback.dao.OrderHistoryMapper;
+import io.cjf.jcartadministrationback.po.Order;
+import io.cjf.jcartadministrationback.po.OrderHistory;
+import io.cjf.jcartadministrationback.service.OrderHistoryService;
+import io.cjf.jcartadministrationback.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class OrderHistoryServiceImpl implements OrderHistoryService {
+
+    @Autowired
+    private OrderHistoryMapper orderHistoryMapper;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Override
+    public List<OrderHistory> getByOrderId(Long orderId) {
+        List<OrderHistory> orderHistories = orderHistoryMapper.selectByOrderId(orderId);
+        return orderHistories;
+    }
+
+    @Override
+    @Transactional
+    public Long create(OrderHistory orderHistory) {
+        orderHistoryMapper.insertSelective(orderHistory);
+        Order order = new Order();
+        order.setOrderId(orderHistory.getOrderId());
+        order.setStatus(orderHistory.getOrderStatus());
+        order.setUpdateTime(new Date());
+        orderService.update(order);
+        Long orderHistoryId = orderHistory.getOrderHistoryId();
+        return orderHistoryId;
+    }
+}
