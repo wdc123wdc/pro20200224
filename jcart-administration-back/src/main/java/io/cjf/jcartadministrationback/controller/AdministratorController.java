ce700788468d568359f9caab7fdbedfe567c0374
wdc <1650428523@qq.com>
2020-03-09 15:26:18 +0800

    找回密码是发送随机验证码

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
@@ -11,10 +11,17 @@ import io.cjf.jcartadministrationback.po.Administrator;
 import io.cjf.jcartadministrationback.service.AdministratorService;
 import io.cjf.jcartadministrationback.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.SecureRandom;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,6 +34,17 @@ public class AdministratorController {
 
     @Autowired
     private JWTUtil jwtUtil;
+
+    @Autowired
+    private SecureRandom secureRandom;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    private Map<String, String> emailPwdResetCodeMap = new HashMap<>();
 
     @GetMapping("/login")
     public AdministratorLoginOutDTO login(AdministratorLoginInDTO administratorLoginInDTO) throws ClientException {
@@ -72,15 +90,23 @@ public class AdministratorController {
     }
 
     @PostMapping("/changePwd")
-    public void changePwd(@RequestBody AdministratorChangePwdInDTO administratorChangePwdInDTO,
-                          @RequestAttribute Integer administratorId){
-
+    public void getPwdResetCode(@RequestParam String email) throws ClientException {
+        Administrator administrator = administratorService.getByEmail(email);
+        if (administrator == null){
+            throw new ClientException(ClientExceptionConstant.ADMINISTRATOR_EMAIL_NOT_EXIST_ERRCODE, ClientExceptionConstant.ADMINISTRATOR_EMAIL_NOT_EXIST_ERRMSG);
+        }
+        byte[] bytes = secureRandom.generateSeed(3);
+        String hex = DatatypeConverter.printHexBinary(bytes);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(email);
+        message.setSubject("jcart管理端管理员密码重置");
+        message.setText(hex);
+        mailSender.send(message);
+        //todo send messasge to MQ
+        emailPwdResetCodeMap.put(email, hex);
     }
 
-    @GetMapping("/getPwdResetCode")
-    public String getPwdResetCode(@RequestParam String email){
-        return null;
-    }
 
     @PostMapping("/resetPwd")
     public void resetPwd(@RequestBody AdministratorResetPwdInDTO administratorResetPwdInDTO){
--- jcart-store-back/src/main/java/io/cjf/jcartstoreback/dao/AdministratorMapper.java
+++ jcart-store-back/src/main/java/io/cjf/jcartstoreback/dao/AdministratorMapper.java
@@ -1,6 +1,10 @@
 package io.cjf.jcartstoreback.dao;
 
+import com.github.pagehelper.Page;
 import io.cjf.jcartstoreback.po.Administrator;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface AdministratorMapper {
     int deleteByPrimaryKey(Integer administratorId);
@@ -14,4 +18,7 @@ public interface AdministratorMapper {
     int updateByPrimaryKeySelective(Administrator record);
 
     int updateByPrimaryKey(Administrator record);
+
+
+
 }
