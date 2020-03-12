baac2c24ff09cf62ddd228dd9ed954594d92bf2d
wdc <1650428523@qq.com>
2020-03-12 15:11:16 +0800

    fix page

--- jcart-administration-front/viewmodels/administrator-index.js
+++ jcart-administration-front/viewmodels/administrator-index.js
@@ -3,7 +3,8 @@ var app = new Vue({
     data: {
         pageInfo: '',
         pageNum: 1,
-        selecteAdministrators: []
+        selectedAdministrators: [],
+        statuses: ['禁用', '启用']
     },
     computed: {
         selectedAdministratorIds() {
