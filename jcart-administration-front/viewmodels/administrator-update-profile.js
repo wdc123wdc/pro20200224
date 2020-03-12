cf65696ab518196ceced0403afeb37924edc5d28
wdc <1650428523@qq.com>
2020-03-12 14:56:25 +0800

    fix administration administrator update profile page

--- jcart-administration-front/viewmodels/administrator-update-profile.js
+++ jcart-administration-front/viewmodels/administrator-update-profile.js
@@ -6,38 +6,39 @@ var app = new Vue({
         realName: '',
         email: '',
         avatarUrl: '',
-        createTimestamp: '',
+        createTimestamp: ''
     },
-    mounted(){
+    mounted() {
         console.log('view mounted');
         this.getMyProfile();
     },
     methods: {
-        handleUpdateClick(){
+        handleUpdateClick() {
             console.log('update click');
             this.updateMyProfile();
         },
-        getMyProfile(){
+        getMyProfile() {
             axios.get('/administrator/getProfile')
-              .then(function (response) {
-                console.log(response);
-                var me = response.data;
-                app.administratorId = me.administratorId;
-                app.username = me.username;
-                app.realName = me.realName;
-                app.email = me.email;
-                app.avatarUrl = me.avatarUrl;
-                app.createTimestamp = me.createTimestamp;
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+                .then(function (response) {
+                    console.log(response);
+                    var me = response.data;
+                    app.administratorId = me.administratorId;
+                    app.username = me.username;
+                    app.realName = me.realName;
+                    app.email = me.email;
+                    app.avatarUrl = me.avatarUrl;
+                    app.createTimestamp = me.createTimestamp;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         },
         updateMyProfile() {
             axios.post('/administrator/updateProfile', {
                 realName: this.realName,
                 email: this.email,
-                avatarUrl: this.avatarUrl
+                avatarUrl: this.avatarUrl,
+
             })
                 .then(function (response) {
                     console.log(response);
