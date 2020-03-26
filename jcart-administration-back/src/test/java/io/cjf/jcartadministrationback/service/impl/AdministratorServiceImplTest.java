65061d14262f134cd7cc145d9555f8ea74eb4ffa
wdc <1650428523@qq.com>
2020-03-26 16:59:58 +0800

    add adminis administtrator service test

---
+++ jcart-administration-back/src/test/java/io/cjf/jcartadministrationback/service/impl/AdministratorServiceImplTest.java
@@ -0,0 +1,24 @@
+package io.cjf.jcartadministrationback.service.impl;
+
+import io.cjf.jcartadministrationback.po.Administrator;
+import io.cjf.jcartadministrationback.service.AdministratorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AdministratorServiceImplTest {
+
+    private AdministratorService administratorService;
+
+    @Test
+    void getByUsername() {
+        String username = "admin";
+        Administrator administrator = administratorService.getByUsername(username);
+        assertNotNull(administrator);
+        username = "admin110";
+        administrator = administratorService.getByUsername(username);
+        assertNull(administrator);
+    }
+}
